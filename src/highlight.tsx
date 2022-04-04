ec1d18a47a1b859a76d4ee04f4c7a79174e8cd3c
Vladyslav Kapkan <vladickbaraza@gmail.com>
2022-04-04 17:50:28 +0200

    fix bug with empty string

--- src/highlight.tsx
+++ src/highlight.tsx
@@ -1,56 +1,52 @@
 import React, { createElement } from 'react'
 
 interface IProps {
-  children: any
-  search?: string
+	children: any
+	search?: string
 }
 
 export default function Highlight({ children, search }: IProps) {
+	if (search === '' || !search) {
+		return children
+	}
 
+	const markSearch = (
+		children: any,
+		HTMLtype?: any
+	): JSX.Element[] | JSX.Element | string => {
+		const isString = typeof children === 'string'
+		const isArray = Array.isArray(children as JSX.Element[])
+		const isElement = !isString && !isArray
+		const regex = new RegExp(search, 'gi')
 
-  if (search === '' || !search) {
-    return children
-  }
-  
-  const markSearch = (
-    children: any,
-    HTMLtype?: any
-  ): JSX.Element[] | JSX.Element | string => {
-    const isString = typeof children === 'string'
-    const isArray = Array.isArray(children as JSX.Element[])
-    const isElement = !isString && !isArray
-    const regex = new RegExp(search, 'gi')
+		if (isString) {
+			if (!search || search === '') {
+				return children
+			}
+			const markedElementString = children.replace(regex, (match: string) => {
+				return `<mark>${match}</mark>`
+			})
 
+			return createElement(HTMLtype || 'span', {
+				dangerouslySetInnerHTML: { __html: markedElementString }
+			})
+		}
 
-    if (!search) {
-      return children
-    }
+		if (isArray) {
+			return children.map((child: JSX.Element) => markSearch(child))
+		}
 
-    if (isString) {
-      const markedElementString = children.replace(regex, (match: string) => {
-        return `<mark>${match}</mark>`
-      })
+		if (isElement) {
+			const { children: childChildren, ...props } = children.props
+			const newChildren = markSearch(childChildren)
+			return createElement(HTMLtype || children?.type, {
+				...props,
+				children: newChildren
+			})
+		}
 
-      return createElement(HTMLtype || 'span', {
-        dangerouslySetInnerHTML: { __html: markedElementString }
-      })
-    }
+		return children
+	}
 
-    if (isArray) {
-      return children.map((child: JSX.Element) => markSearch(child))
-    }
-
-    if (isElement) {
-      const { children: childChildren, ...props } = children.props
-      const newChildren = markSearch(childChildren)
-      return createElement(HTMLtype || children?.type, {
-        ...props,
-        children: newChildren
-      })
-    }
-
-    return children
-  }
-
-  return <>{markSearch(children)}</>
+	return <>{markSearch(children)}</>
 }
